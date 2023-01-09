ef2898e97461f29f13065aae791a94ec0826bcf4
kz7 <zhao7720@163.com>
2023-01-09 20:52:28 +0800

    First Time
    Create Repo

---
+++ src/main/java/com/hankshop/shinobik/ShinobiKApplication.java
@@ -0,0 +1,13 @@
+package com.hankshop.shinobik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShinobiKApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShinobiKApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hankshop/shinobik/ShinobiKApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hankshop.shinobik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShinobiKApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
